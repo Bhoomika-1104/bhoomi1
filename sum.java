e8d102bc2eb58243e6f90cb41c2a4eefc0bdbeee
Bhoomika-1104 <devipriyabhoomikacharugundla@gmail.com>
2024-12-11 16:14:02 +0530

    Update sum.java

--- sum.java
+++ sum.java
@@ -2,16 +2,8 @@ import java.util.Scanner;
 
 public class SumOfTwoNumbers {
     public static void main(String[] args) {
-        // Create a Scanner object for user input
-        Scanner scanner = new Scanner(System.in);
-
-        // Prompt the user for the first number
-        System.out.print("Enter the first number: ");
-        int num1 = scanner.nextInt();
-
-        // Prompt the user for the second number
-        System.out.print("Enter the second number: ");
-        int num2 = scanner.nextInt();
+       int num1=15;
+        int num2=20;
 
         // Calculate the sum
         int sum = num1 + num2;
